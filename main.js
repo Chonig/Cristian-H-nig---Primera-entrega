c37f47a75aec7da3f30841bae606b322de10ce61
Cristian Honig <cristian.honig@gmail.com>
2022-11-29 18:08:27 -0300

    Primera Entrega

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("Bienvenido, Ingrese su nombre para continuar");
+let nombreSaludo = prompt("Ingrese su nombre:");
+
+function saludo (){
+    return alert("Hola "  +  nombreSaludo  +  "  Bienvenido al simulador de cuotas, aqui podras calcular los interes de un monto final ");
+}
+
+saludo()
